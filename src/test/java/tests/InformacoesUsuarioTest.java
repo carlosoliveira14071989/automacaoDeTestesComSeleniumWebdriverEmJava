60b98fae2bca66193974222f13c62914b5fbce78
Carlos <carlosoliveira1989@hotmail.com>
2020-04-25 15:02:20 -0300

    atualizando

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -2,8 +2,12 @@ package tests;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class InformacoesUsuarioTest {
     @Test
@@ -15,16 +19,24 @@ public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
         navegador.manage().window().maximize();
 
         //Clicar no link com o texto "Sign in"
+        navegador.findElement(By.linkText("Sign in")).click();
 
         //Preencher o campo login com "julio0001" dentro do formulario de id "signinbox"
-
+        WebElement formularioSigninBox = navegador.findElement(By.id("signinbox"));
+        formularioSigninBox.findElement(By.name("login")).sendKeys("julio0001");
         //Preencher o password com "123456" dentro do formulario de id "signinbox"
+        formularioSigninBox.findElement(By.name("password")).sendKeys("123456");
 
         //Clicar no link com o texto "SIGN IN"
+         navegador.findElement(By.linkText("SIGN IN")).click();
 
         //Validar que dentro do elemento com class "me" possui o texto "Hi, Julio"
+        navegador.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String texto = navegador.findElement(By.className("me")).getText();
+        assertEquals("Hi, Julio",texto);
 
         //Fechar o navegador
+        navegador.quit();
 
 
 
