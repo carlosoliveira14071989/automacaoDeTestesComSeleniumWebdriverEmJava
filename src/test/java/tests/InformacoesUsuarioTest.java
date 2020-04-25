ea40a02f47897c0af31e0037f459c7aaa1962e2b
Carlos <carlosoliveira1989@hotmail.com>
2020-04-25 01:30:39 -0300

    atualizando

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -1,9 +1,33 @@
 package tests;
 
-public class InformacoesUsuarioTest {
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class InformacoesUsuarioTest {
     @Test
+
 public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Carlito\\drivers\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver();
+        navegador.get("http://www.juliodelima.com.br/taskit/");
+        navegador.manage().window().maximize();
+
+        //Clicar no link com o texto "Sign in"
+
+        //Preencher o campo login com "julio0001" dentro do formulario de id "signinbox"
+
+        //Preencher o password com "123456" dentro do formulario de id "signinbox"
+
+        //Clicar no link com o texto "SIGN IN"
+
+        //Validar que dentro do elemento com class "me" possui o texto "Hi, Julio"
+
+        //Fechar o navegador
+
+
+
 
 
     }
